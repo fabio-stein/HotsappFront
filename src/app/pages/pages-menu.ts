9a1bdc98a7fc8a6e4f1f2b6ddccef12ffdcf413c
Fabio <fabiolux999@gmail.com>
2019-08-26 22:42:25 -0300

    Change menu icons

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -3,12 +3,13 @@ import { NbMenuItem } from '@nebular/theme';
 export const MENU_ITEMS: NbMenuItem[] = [
   {
     title: 'Dashboard',
-    icon: 'home-outline',
+    icon: 'home',
     link: '/pages/dashboard',
     home: true,
   },
   {
     title: 'Channels',
+    icon: 'list',
     link: 'channel',
   },
   {
