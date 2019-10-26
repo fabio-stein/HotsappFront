41074144878aa0c6ad7926cb1f00b3d451642f82
Fabio <fabiolux999@gmail.com>
2019-10-25 22:25:40 -0300

    Contacts auto refresh

--- src/app/pages/chat/components/contact-list/contact-list.component.ts
+++ src/app/pages/chat/components/contact-list/contact-list.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { ChatService } from '../../chat.service';
+import { Subscription, timer } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.scss']
 })
-export class ContactListComponent implements OnInit {
+export class ContactListComponent implements OnInit, OnDestroy {
   @Input()
   currentNumber: string;
   @Output()
@@ -15,18 +17,44 @@ export class ContactListComponent implements OnInit {
   isAddingContact = false;
   newContactNumber = null;
   hasUnsavedContact = false;
+  isUpdating = false;
+  subscription: Subscription;
 
   constructor(private service: ChatService) { }
 
-  async ngOnInit() {
-    let e = await this.service.GetContactUpdate(this.currentNumber);
-    this.contacts = [];
+  ngOnInit() {
+    this.subscription = timer(0, 1000).pipe(
+      switchMap(() => this.getUpdate())
+    ).subscribe();
+  }
 
-    let list = e.map(function (n) {
-      return { numberId: n.numberId, picture: 'https://api.adorable.io/avatars/50/' + n.numberId, title: n.contactDateUTC }
-    });
-    this.contacts = list;
-    console.log(e);
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  async getUpdate() {
+    if (this.isUpdating) { return; }
+    try {
+      this.isUpdating = true;
+      let e = await this.service.GetContactUpdate(this.currentNumber);
+      this.contacts = [];
+
+      let list = e.map(function (n) {
+        return { numberId: n.numberId, picture: 'https://api.adorable.io/avatars/50/' + n.numberId, title: n.contactDateUTC }
+      });
+
+      if (this.hasUnsavedContact) {
+        let index = list.findIndex(n => n.numberId == this.newContactNumber)
+        if (index != -1) {
+          list.splice(index, 1);
+        }
+      }
+
+      this.contacts = list;
+    } catch (e) {
+      console.log(e);
+    }
+    this.isUpdating = false;
   }
 
   contacts = [];
