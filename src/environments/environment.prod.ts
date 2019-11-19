61014f67eb469e45c43050e06f9729c6c000bb68
Fabio <fabiolux999@gmail.com>
2019-11-19 20:55:39 -0300

    Migrate to new api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: 'https://api.hotsapp.net:5000',
+  API_ENDPOINT: 'https://api.hotsapp.net',
   paypalClientId: 'AQdu2Q3IOgK-68-H4JgKIyadz9PJAZqyFkHFZCdAxxyiKm6L0yf6ZJKZMFrHIjp9KzrT1W9w4lISj7cW',
   firebase: {
     apiKey: "AIzaSyBwp44GfqNAUZnhAT4zTrkS2t68CHbNN2M",
