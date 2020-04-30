5c735a3c844e7255a04bbdf9e189f98c1af8d62a
Fabio <fabiolux999@gmail.com>
2020-04-29 21:01:56 -0300

    Add Playlist Dashboard Page (#5)

--- src/app/pages/channel/views/channel-page-playlist/channel-page-playlist.component.ts
+++ src/app/pages/channel/views/channel-page-playlist/channel-page-playlist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ChannelService } from '../../channel.service';
 import { PlaylistItemView } from './model/PlaylistItemView';
 import { ActivatedRoute } from '@angular/router';
+import { NbToastrService } from '@nebular/theme';
+import { ConfirmDialogService } from '../../../../@theme/components/confirm-dialog/confirm-dialog.service';
 
 @Component({
   selector: 'channel-page-playlist',
@@ -11,11 +13,19 @@ import { ActivatedRoute } from '@angular/router';
 export class ChannelPagePlaylistComponent {
   Channel: number;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private toastr: NbToastrService, private confirmDialog: ConfirmDialogService) {
     this.activatedRoute.parent.paramMap.subscribe(params => {
       let id = params.get("channelId");
       this.Channel = Number(id);
     });
   }
 
+  async removeItem() {
+    let confirm = await this.confirmDialog.ConfirmAsync("Remove Media", "Are you sure?");
+    if (confirm == true) {
+      this.toastr.success("Media successfully removed", "Success");
+    } else {
+      this.toastr.danger("Failed to remove media", "Error");
+    }
+  }
 }
