338ed66ea96352cec84628f3b46a61062aad4cc6
Fabio <fabiolux999@gmail.com>
2020-07-11 23:30:30 -0300

    Update footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Hotsapp &copy; 2020</span>
+    <span class="created-by">Plister &copy; 2020</span>
     
     <div class="socials">
-      <a href="https://www.facebook.com/hotsappbrasil" target="_blank"><i class="fab fa-facebook"></i></a>
-      <a href="https://www.instagram.com/hotsapp_br" target="_blank"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.facebook.com/plister.official" target="_blank"><i class="fab fa-facebook"></i></a>
+      <a href="https://www.instagram.com/plister.app" target="_blank"><i class="fab fa-instagram"></i></a>
     </div>
   `,
 })
