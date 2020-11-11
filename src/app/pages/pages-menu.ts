0c4261a65e2ceddd478a372e6d83f3d8d1d23456
Fabio <fabiolux999@gmail.com>
2020-11-11 14:54:32 -0300

    Clear UI Tests

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -8,53 +8,59 @@ export const MENU_ITEMS: NbMenuItem[] = [
     home: true,
   },
   {
-    title: 'Campanhas',
-    icon: 'clipboard',
-    link: '/pages/campaign'
-  },
-  {
     title: 'Atendimentos',
     icon: 'comments',
     link: '/pages/chat'
   },
   {
-    title: 'Números',
-    icon: 'mobile-alt',
+    title: 'Teste',
+    icon: 'clipboard',
     children: [
       {
-        title: 'Meus Números',
-        link: '/pages/number/my'
-      }, {
-        title: 'Adicionar Número',
-        link: '/pages/number/connector'
-      }, {
-        title: 'Comprar Novo',
-        link: '/pages/number/buy'
-      }
-    ]
-  },
-  {
-    title: 'Assinatura',
-    icon: 'credit-card',
-    children: [
-      {
-        title: 'Minha Assinatura',
-        link: '/pages/subscription'
+        title: 'Carteira',
+        icon: 'wallet',
+        link: '/pages/wallet',
       },
       {
-        title: 'Planos',
-        link: '/pages/subscription/plans'
-      }
+        title: 'Empresa',
+        icon: 'building',
+        link: '/pages/company/config',
+      },
+      {
+        title: 'Assinatura',
+        icon: 'credit-card',
+        children: [
+          {
+            title: 'Minha Assinatura',
+            link: '/pages/subscription'
+          },
+          {
+            title: 'Planos',
+            link: '/pages/subscription/plans'
+          }
+        ]
+      },
+      {
+        title: 'Números',
+        icon: 'mobile-alt',
+        children: [
+          {
+            title: 'Meus Números',
+            link: '/pages/number/my'
+          }, {
+            title: 'Adicionar Número',
+            link: '/pages/number/connector'
+          }, {
+            title: 'Comprar Novo',
+            link: '/pages/number/buy'
+          }
+        ]
+      },
+      {
+        title: 'Campanhas',
+        icon: 'clipboard',
+        link: '/pages/campaign'
+      },
     ]
-  },
-  {
-    title: 'Carteira',
-    icon: 'wallet',
-    link: '/pages/wallet',
-  },
-  {
-    title: 'Empresa',
-    icon: 'building',
-    link: '/pages/company/config',
   },
 ];
